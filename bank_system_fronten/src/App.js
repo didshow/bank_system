a342535fb0346fa0a1bcb1dec11d1d1a72eceecd
xionglingxin <1437079420@qq.com>
2024-05-29 20:24:52 +0800

    connect wallet

--- bank_system_fronten/src/App.js
+++ bank_system_fronten/src/App.js
@@ -2,13 +2,44 @@
 import './App.css';
 import Navbar from './Navbar.js';
 import { useState, useEffect } from 'react';
+import UploadSuccess from './UploadSuccess.js';
 
  
 
 function App() {
+  const [walletAddress,setWalletAddress] = useState('');
+  useEffect(()=>{
+  //  getWalletAddress();
+  addWalletListener();
+  })
+
+
+  function addWalletListener(){
+    if(window.ethereum){
+      window.ethereum.on('accountsChanged', function(accounts){
+        if(accounts.length>0){
+          setWalletAddress(accounts[0]);
+        }else{
+          setWalletAddress('');
+        }
+      });
+    }else{
+      console.log('Please install metamask');
+    }
+  }
+  async function getWalletAddress(){
+    if(window.ethereum){
+      const accounts = await window.ethereum.request({method:'eth_requestAccounts'});
+      setWalletAddress(accounts[0]);
+    }else{
+      alert('Please install metamask');
+    }
+  }
+
   return (
     <div className="container">
-      <Navbar />
+      <Navbar onConnectWallet={getWalletAddress} walletAddress={walletAddress}/>
+     <UploadSuccess/>
     </div>
   );
 }
--- bank_system_fronten/src/Navbar.js
+++ bank_system_fronten/src/Navbar.js
@@ -1,11 +1,11 @@
-function Navbar() {
+function Navbar({onConnectWallet,walletAddress}) {
     return(
         <nav className="navbar">
             <div className="nav-brand">
                 BankOnline
             </div>
             <div className="navbar-menu ">
-                <button className="connect-wallet-button">Connect Wallet</button>
+                <button className="connect-wallet-button"onClick={onConnectWallet}>{walletAddress.slice(0,8)||"Connect Wallet"}</button>
             </div>
         </nav>
     )
