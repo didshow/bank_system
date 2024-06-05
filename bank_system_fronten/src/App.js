58b9091b886ccb466d507fc5a79de2aa0dbd085c
Annxxnn <2206472629@qq.com>
2024-06-05 21:58:48 +0800

    fronten-home and router

--- bank_system_fronten/src/App.js
+++ bank_system_fronten/src/App.js
@@ -1,52 +1,51 @@
- import './App.css';
+import './App.css';
 import Navbar from './component/Navbar.js';
 import Home from './component/Home';
+import Service from './component/Service.js';
 import { useState, useEffect } from 'react';
-import {Router,Route,Routes} from 'react-router-dom';
-import axios from 'axios';
- 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import axios from 'axios';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [walletAddress,setWalletAddress] = useState('');
-  useEffect(()=>{
-  //  getWalletAddress();
-  addWalletListener();
+  const [walletAddress, setWalletAddress] = useState('');
+  useEffect(() => {
+    //  getWalletAddress();
+    addWalletListener();
   })
 
 
-  function addWalletListener(){
-    if(window.ethereum){
-      window.ethereum.on('accountsChanged', function(accounts){
-        if(accounts.length>0){
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', function (accounts) {
+        if (accounts.length > 0) {
           setWalletAddress(accounts[0]);
-        }else{
+        } else {
           setWalletAddress('');
         }
       });
-    }else{
+    } else {
       console.log('Please install metamask');
     }
   }
-  async function getWalletAddress(){
-    if(window.ethereum){
-      const accounts = await window.ethereum.request({method:'eth_requestAccounts'});
+  async function getWalletAddress() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
       setWalletAddress(accounts[0]);
-    }else{
+    } else {
       alert('Please install metamask');
     }
   }
 
   return (
     <div className="container">
-    <Navbar onConnectWallet={getWalletAddress} walletAddress={walletAddress}/>
-      {/* <Router>
-        
-        <Routes> 
-          <Route path='/' element={<Home />} />
+      <Router>
+        <Navbar onConnectWallet={getWalletAddress} walletAddress={walletAddress} />
+        <Routes>
+          <Route path='/Service' element={<Service />} />
+          <Route path='/' element={<Home onConnectWallet={getWalletAddress} walletAddress={walletAddress} />} />
         </Routes>
-      </Router> */}
-      
-     
+      </Router>
     </div>
   );
 }
