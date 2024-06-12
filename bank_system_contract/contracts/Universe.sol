eec470600d8f14b3c255f47b0c5c903c894b8d21
Annxxnn <2206472629@qq.com>
2024-06-12 22:03:56 +0800

    universe.sol

--- bank_system_contract/contracts/Universe.sol
+++ bank_system_contract/contracts/Universe.sol
@@ -5,29 +5,38 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Universe {
     // --- Auth ---
-    mapping (address => uint) public wards;
-    function rely(address usr) external auth { require(live == 1, "Universe/not-live"); wards[usr] = 1; }
-    function deny(address usr) external auth { require(live == 1, "Universe/not-live"); wards[usr] = 0; }
-    modifier auth {
+    mapping(address => uint) public wards;
+    function rely(address usr) external auth {
+        require(live == 1, "Universe/not-live");
+        wards[usr] = 1;
+    }
+    function deny(address usr) external auth {
+        require(live == 1, "Universe/not-live");
+        wards[usr] = 0;
+    }
+    modifier auth() {
         require(wards[msg.sender] == 1, "Vat/not-authorized");
         _;
     }
 
-    mapping(address => mapping (address => uint)) public can;
-    function hope(address usr) external { can[msg.sender][usr] = 1; }
-    function nope(address usr) external { can[msg.sender][usr] = 0; }
+    mapping(address => mapping(address => uint)) public can;
+    function hope(address usr) external {
+        can[msg.sender][usr] = 1;
+    }
+    function nope(address usr) external {
+        can[msg.sender][usr] = 0;
+    }
     function wish(address bit, address usr) internal view returns (bool) {
         return bit == usr || can[bit][usr] == 1;
     }
 
-    
     // --Data--
     mapping(address => uint256) public stars;
-    
-    IERC20  public token;                        // only use star token
-    uint256 public live;                        // Active Flag
-    uint256 public totalStars;                  // total stars
-    uint256 public reserve;                     // reserve balance
+
+    IERC20 public token; // only use star token
+    uint256 public live; // Active Flag
+    uint256 public totalStars; // total stars
+    uint256 public reserve; // reserve balance
 
     // --event--
     event Deposit(address indexed user, uint256 amount);
@@ -35,28 +44,32 @@ contract Universe {
     event AddResrves(address indexed from, uint256 amount);
     event MoveResrves(address indexed to, uint256 amount);
     event Transfer(address indexed src, address indexed dst, uint256 amount);
-    
-    
+
     constructor(address token_addr) {
         wards[msg.sender] = 1;
         live = 1;
         token = IERC20(token_addr);
     }
-    
-  
+
     // --bank logic--
     function deposit(address to, uint256 amount) external {
         require(live == 1, "Universe/not-live");
-        require(token.transferFrom(msg.sender, address(this), amount), "transferFrom failed");
+        require(
+            token.transferFrom(msg.sender, address(this), amount),
+            "transferFrom failed"
+        );
         stars[to] += amount;
         totalStars += amount;
         emit Deposit(to, amount);
     }
 
-    function withdraw(address src,address to, uint256 amount) external {
+    function withdraw(address src, address to, uint256 amount) external {
         require(live == 1, "Universe/not-live");
-        require(totalStars >= amount || stars[msg.sender] >= amount, "Universe/not-enough-stars");
-        require(wish(msg.sender, src),"Universe/account-not-authorized");
+        require(
+            totalStars >= amount || stars[msg.sender] >= amount,
+            "Universe/not-enough-stars"
+        );
+        require(wish(msg.sender, src), "Universe/account-not-authorized");
         require(token.transfer(to, amount), "transferFrom failed");
         stars[src] -= amount;
         totalStars -= amount;
@@ -71,7 +84,7 @@ contract Universe {
         emit Transfer(src, dst, amount);
     }
 
-    function move(address src, address dst, uint256 amount) external auth{
+    function move(address src, address dst, uint256 amount) external auth {
         stars[src] -= amount;
         stars[dst] += amount;
         emit Transfer(src, dst, amount);
@@ -82,7 +95,7 @@ contract Universe {
         require(stars[msg.sender] >= amount, "Universe/not-enough-stars");
         stars[msg.sender] -= amount;
         reserve += amount;
-        emit Transfer(msg.sender , address(this), amount);
+        emit Transfer(msg.sender, address(this), amount);
         emit AddResrves(msg.sender, amount);
     }
 
@@ -90,13 +103,11 @@ contract Universe {
         require(reserve >= amount, "Universe/not-enough-reserve");
         stars[to] += amount;
         reserve -= amount;
-        emit Transfer(msg.sender , to, amount);
+        emit Transfer(msg.sender, to, amount);
         emit MoveResrves(to, amount);
-        
     }
 
     function cage() external auth {
         live = 0;
     }
-
 }
